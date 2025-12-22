b924e0a0a2603ad246a3bea29353ae73c546f5c8
Yongjooon <95464174+Yongjooon@users.noreply.github.com>
2025-12-22 16:51:55 +0900

    [feat] 부동산 front 제작
    - 부동산 기능 front api 연결

--- src/api/BudongApi.js
+++ src/api/BudongApi.js
@@ -9,7 +9,7 @@ const headers = {
 export default {
     async getMyArticles(page=0, limit=15) {
         const response = await refreshToken.checkToken();
-        const { data } = await api.get(`${BASE_URL}/v1/search/preference`, {params: { page, limit }});
+        const { data } = await api.get(`${BASE_URL}/article/home-issues`);
         return data;
     },
 
