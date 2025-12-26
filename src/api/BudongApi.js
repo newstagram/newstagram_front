b8a46ef37b9cd9deba5402c04fcd216d65a136a4
김현목 <dabinchi388@naver.com>
2025-12-26 09:44:25 +0900

    [fix]

--- src/api/BudongApi.js
+++ src/api/BudongApi.js
@@ -9,7 +9,17 @@ const headers = {
 export default {
   async getMyArticles(page = 0, limit = 15) {
     const response = await refreshToken.checkToken();
-    const { data } = await api.get(`${BASE_URL}/article/home-issues`);
+    // const { data } = await api.get(`${BASE_URL}/article/home-issues`);
+    const { data } = await api.post(
+  `${BASE_URL}/v1/search/test`,
+  {
+    query: "부동산 아파트 빌라 상가 오피스텔 부동산투자 매매 임대차 시세 전세 월세 매매가 주택청약 부동산분양 재개발 종부세 집값",
+    limit: 10,
+    page: 0,
+  }
+);
+
+    
     return data;
   },
 };
